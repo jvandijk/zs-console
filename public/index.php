2401d733c218d517ddf38ffbb5379fea38a1d310
Jeroen van Dijk <jeroen@enrise.com>
2013-02-09 11:10:47 +0100

    Updating skeleton application to reflect ZF2.1 structure

--- public/index.php
+++ public/index.php
@@ -1,21 +1,6 @@
 <?php
 chdir(dirname(__DIR__));
-require_once (getenv('ZF2_PATH') ?: 'vendor/ZendFramework/library') . '/Zend/Loader/AutoloaderFactory.php';
-Zend\Loader\AutoloaderFactory::factory();
+require_once('vendor/autoload.php');
 
-$appConfig = include 'config/application.config.php';
-
-$listenerOptions  = new Zend\Module\Listener\ListenerOptions($appConfig['module_listener_options']);
-$defaultListeners = new Zend\Module\Listener\DefaultListenerAggregate($listenerOptions);
-$defaultListeners->getConfigListener()->addConfigGlobPath("config/autoload/{,*.}{global,local}.config.php");
-    
-
-$moduleManager = new Zend\Module\Manager($appConfig['modules']);
-$moduleManager->events()->attachAggregate($defaultListeners);
-$moduleManager->loadModules();
-
-// Create application, bootstrap, and run
-$bootstrap   = new Zend\Mvc\Bootstrap($defaultListeners->getConfigListener()->getMergedConfig());
-$application = new Zend\Mvc\Application;
-$bootstrap->bootstrap($application);
-$application->run()->send();
+// Run the application!
+Zend\Mvc\Application::init(require 'config/application.config.php')->run();
